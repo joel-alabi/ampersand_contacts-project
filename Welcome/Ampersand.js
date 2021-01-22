2066668591aebf6b5d10c993a63215de92fdd029
Joel Alabi <joelalabi45@gmail.com>
2021-01-22 10:38:32 +0000

    initialised first screen

--- Welcome/Ampersand.js
+++ Welcome/Ampersand.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View } from 'react-native'; 
+import { StyleSheet, Text, View,Image,TouchableOpacity } from 'react-native'; 
 export default class Ampersand extends Component {
     render() {
         return (
             <View style={styles.welcome}>
-               <Text>how are you</Text> 
+               <Image source={require('../assets/ampersand-removebg-preview.png')} style={styles.Image}/>
+               <View style={styles.Ampersand}>
+                   <Text style={styles.text}>AMPERSAND</Text>
+                   <Text style={styles.contact}>CONTACT</Text>
+               </View>
+               <TouchableOpacity style={styles.getstarted}>
+                   <Text style={{fontWeight:'300', fontSize:17}}>GET STARTED</Text>
+                   <Text style={styles.empty}></Text>
+               </TouchableOpacity>
             </View>
         )
     }
@@ -13,7 +21,34 @@ const styles = StyleSheet.create({
     welcome: {
       backgroundColor: '#fff',
       alignItems: 'center',
-      justifyContent: 'center',
+      marginTop:130
+    },
+    Image:{
+        height:70,
+        width:250
+    },
+    Ampersand:{
+    textAlign:'center',
+    marginTop:130,
+    },
+    text:{
+        fontSize:25,
+        fontWeight:'bold',
+        color:'gray'
+    },
+    contact:{
+        fontWeight:'bold',
+        fontSize:25,
+        paddingLeft:15,
+        color:'gray'
+    },
+    getstarted:{
+marginTop:200,
+    },
+    empty:{
+        height:2,
+        width:90,
+        backgroundColor:'red'
     },
   });
   
--- Welcome/WelcomeScreen.js
+++ Welcome/WelcomeScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 export default class WelcomeScreen extends Component {
     render() {
